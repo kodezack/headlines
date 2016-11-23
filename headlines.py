e41e5ab235fba3170ad9de45056b7f1f8432f2af
kodezack <kodezack@gmail.com>
2016-11-23 16:59:34 +0700

    FeedParser & return html 4,get_news with params

--- headlines.py
+++ headlines.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+BBC_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
         'cnn':'http://rss.cnn.com/rss/edition.rss',
         'fox':'http://feeds.foxnews.com/foxnews/latest',
         'iol':'http://www.iol.co.za/cmlink/1.640'}
@@ -23,12 +23,12 @@ def get_news(publication):
     return """
         <html>
             <body>
-                <h1> BBC Headlines </h1>
-                <b> {0} </b> <br/>
-                <i> {1} </i> <br/>
-                <p> {2} </p> <br/>
+                <h1> {0} Headlines </h1>
+                <b> {1} </b> <br/>
+                <i> {2} </i> <br/>
+                <p> {3} </p> <br/>
             </body>
-        </html>""".format(first_article.get("title"),first_article.get("published"), first_article.get("summary"))
+        </html>""".format(publication.upper(),first_article.get("title"),first_article.get("published"), first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
